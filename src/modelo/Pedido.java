c4aecdf56894638e9cbe767ec96ec64aea36fb4e
Daniel Hernandez <d.hernandez24@uniandes.edu.co>
2022-02-09 22:13:06 -0500

    Cambios en la clase Pedido

--- src/modelo/Pedido.java
+++ src/modelo/Pedido.java
@@ -1,6 +1,31 @@
 package modelo;
 
+import java.util.LinkedList;
+
 public class Pedido
 {
-
+	public static int numeroPedidos;
+	private int idPedido;
+	private String nombreCliente;
+	private String direccionCliente;
+	private LinkedList<Producto> itemsPedido;
+	
+	public Pedido(String nombreCliente, String direccionCliente)
+	{
+		this.nombreCliente = nombreCliente;
+		this.direccionCliente = direccionCliente;
+	}
+	
+	public int getIdPedido()
+	{
+		return idPedido;
+	}
+	
+	public void agregarProducto(Producto nuevoItem)
+	{
+		itemsPedido.add(nuevoItem);
+	}
+	
+	
+	
 }
